313315aa0178cfcc7f94abe4414b641810275021
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-02 16:57:00 -0600

    02 - Add repeatString function
    Returns string repeated num times - negative values return "ERROR".

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    // Avoid adding empty string to itself
+    if (!string) {
+        return "";
+    }
+
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
