4411d22efa9dc7168892423b092fa833724ec129
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-03 11:52:00 -0600

    03 - Add reverseString function
    Works backwards through a string, adding each character to output in
    reverse order.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (!string) {
+        return "";
+    }
 
+    // Work backwards through string and add each char in that order
+    let reversed = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
