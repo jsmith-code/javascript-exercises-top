75bfb55b82217dfb75c354fb1427c1b0ac8a7a3d
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-04 12:33:10 -0600

    04 - Add removeFromArray function
    Takes in an array and variable number of targets to remove, then filters
    out each target - only removes strictly equal matches.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    // Check if no elements to remove
+    if (!targets) {
+        return arr;
+    }
+
+    for (target of targets) {
+        arr = arr.filter(element => element !== target);
+    }
+    return arr;
 };
 
 // Do not edit below this line
