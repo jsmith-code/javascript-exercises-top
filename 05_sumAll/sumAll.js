4581f74f11b5a259071684d3e609cde911eb5ca8
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-05 10:39:19 -0600

    05_sumAll: Add error checking for function
    sumAll returns "ERROR" when either inputs are negative or not a number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
 };
 
