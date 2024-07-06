1e4ccd750ea9b0ca94155ad3b392e7df90b6e138
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-05 19:16:40 -0600

    05_sumAll: Add summing functionality
    Function takes in two inputs and sums the numbers in-between them -
    inputs can be in any order.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,6 +6,21 @@ const sumAll = function(a, b) {
         return "ERROR";
     }
 
+    let bigger;
+    let smaller;
+    if (a > b) {
+        bigger = a;
+        smaller = b;
+    } else {
+        bigger = b;
+        smaller = a;
+    }
+
+    let sum = 0;
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
