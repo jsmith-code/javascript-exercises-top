08ca0ef15e47aa46c7683ce1052768e53725d3ec
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-09 11:24:40 -0600

    07_tempConversion: Add function to convert Fahrenheit to Celsius
    Function rounds output to one decimal place (i.e. 95.555 to 95.6)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  // Formula for converting F° (Fahrenheit) to C° (Celsius)
+  // See: https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature
+  let C = (F - 32) * 5/9;
+
+  // Round to one decimal place
+  return Math.round(C * 10) / 10;
 };
 
 const convertToFahrenheit = function() {
