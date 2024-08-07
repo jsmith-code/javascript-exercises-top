04c68bfeb5f19d4c5df33d8be04dd59f9825a987
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-08-07 17:33:19 -0600

    09_palindromes: Add function to check if string is palindrome
    Function compares only alphanumeric characters - all other chars are
    ignored. Function does not recognize characters that cannot be
    capitalized (e.g. Chinese, Arabic), they are ignored.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let parsed = str.toLowerCase()
+                    .split('')
+                    .filter(c => (c.toUpperCase() != c) || ('0' <= c && c <= '9'))
+                    .join('');
 
+    let halfParsedLen = Math.floor(parsed.length / 2);
+
+    for (let i = 0; i < halfParsedLen; i++) {
+        let opposite = (parsed.length - 1) - i;
+        if (parsed[i] !== parsed[opposite]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
