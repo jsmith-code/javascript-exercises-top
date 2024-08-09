706ec7d6125eb6d42d2fe27c727fcd71387ff5ee
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-08-09 07:12:27 -0600

    10_fibonacci: Add function to get nth term of fibonacci sequence
    Function takes input n and returns the nth member of the fibonacci
    sequence, using 0, 1 as starting numbers.
    
    For more info, see:
    https://en.wikipedia.org/wiki/Fibonacci_sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let sequence = [0, 1];
 
+    for (let i = 0; i < n; i++) {
+        sequence.push(sequence[0] + sequence[1]);
+        sequence.shift();
+    }
+    return sequence[0];
 };
 
 // Do not edit below this line
