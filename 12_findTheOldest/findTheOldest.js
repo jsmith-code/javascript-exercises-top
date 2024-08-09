94ab7eb8a2e7f06c590ac90cccd881f4354ea3ba
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-08-09 08:47:58 -0600

    12_findTheOldest: Add function to get oldest person
    Function takes array of people as input, and outputs the person with the
    most years between birth to death. If a death date is not specified for
    a person, the years between their birth and the current year are used.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        let personAge = getAge(person);
+        let oldestPersonAge = getAge(oldestPerson);
 
+        return personAge > oldestPersonAge ? person : oldestPerson;
+    });
 };
+
+const getAge = function(person) {
+    if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        // If no yearOfDeath, assume person is alive and get years from birth to today
+        return new Date().getFullYear() - person.yearOfBirth;
+    } 
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
