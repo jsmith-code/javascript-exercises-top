db0ac7b4bbdedeae2bb0f2096fb3b215320aad85
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-08-09 07:56:15 -0600

    10_fibonacci: Add comment explanation for fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
 const fibonacci = function(n) {
+    // Each number in the fibonacci sequence
+    // is the sum of the previous two numbers:
+    // 0, 1, 1, 2, 3, 5, 8, 13...
+    // 
+    // Starting numbers can vary. For more info, see:
+    // https://en.wikipedia.org/wiki/Fibonacci_sequence
+
     if (n < 0) return "OOPS";
     
+    // Starting numbers
     let sequence = [0, 1];
 
     for (let i = 0; i < n; i++) {
